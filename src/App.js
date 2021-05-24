938bd66326e90c09848ecae2757586055db7a2c0
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-05-24 17:19:10 +0300

    create App, pages.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes'
+import Navbar from './client/Navba/Navbar';
+import Footer from './Footer/Footer';
+
+
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Navbar />
+      <Routes />
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
