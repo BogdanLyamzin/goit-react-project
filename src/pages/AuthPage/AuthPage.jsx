bf70a08557bcf3106388c2b23845aeec3afcfcbd
Roman Bilousov <pomafess@gmail.com>
2021-05-25 15:53:57 +0300

    fix css

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -15,28 +15,30 @@ const AuthPage = () => {
 
     return (
         <>
-            <section className={s.container}>
+            <section>
+                <div  className={s.container}>
                 <div className={s.wrapper}>
-                    <div className={s.containerFoto}>
-                            <div className={s.imgContainer}>
-                                <img src={foto1} alt="family"></img>
-                            </div>
-                            <div className={s.imgContainer}>
-                                <img src={foto2} alt="robot"></img>
-                            </div>
-                            <div className={s.imgContainer}>
-                                <img src={foto3} alt="gamepad"></img>
-                            </div>
-                            <div className={s.imgContainer}>
-                                <img src={foto4} alt="family"></img>
-                            </div>
-                    </div>
-
-                    <div className={s.form}>
-                        <h1 className={s.title}>Выполняй задания,<br/> получи классные призы!</h1>
-                        <LoginForm />
+                <div className={s.containerFoto }>
+                        <div className={s.imgContainer}>
+                            <img src={foto1} alt="family"></img>
+                        </div>
+                        <div className={s.imgContainer}>
+                            <img src={foto2} alt="robot"></img>
+                        </div>
+                        <div className={s.imgContainer}>
+                            <img src={foto3} alt="gamepad"></img>
+                        </div>
+                        <div className={s.imgContainer}>
+                            <img src={foto4} alt="family"></img>
                         </div>
                 </div>
+
+                <div className={s.form}>
+                    <h1 className={s.title}>Выполняй задания,<br/> получи классные призы!</h1>
+                    <LoginForm />
+                </div>
+                </div>
+            </div>
             </section>
         </>
     )
