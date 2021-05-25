5d81f9195890e7e9ea6c7b7ee4997268d43b5e38
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-05-25 11:08:05 +0300

    added styles for shared components

--- src/shared/components/FormCheckbox/FormCheckbox.jsx
+++ src/shared/components/FormCheckbox/FormCheckbox.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid';
 
-import styles from './FormCheckbox.module.sass'
+import styles from './FormCheckbox.module.scss'
+
 
 
 const FormCheckbox = ({ label, className, ...props}) => {
     const id = v4();
 
     return (
-        <div>
-            {label && <label htmlFor={id}>{label}</label>}
-            <input id={id} className={`${styles.checkboxInput}  ${className}`} type="checkbox" {...props }/>
-        </div>
+          <div className={styles.checkbox}>
+            <input id={id} className={ `${styles.checkboxInputHidden} ${className}`} type="checkbox"/>
+            <label aria-hidden="true" className={styles.checkboxInput} htmlFor={id}>
+                <span className={styles.checkboxIcon}></span>
+            </label>
+          </div>
             )
 	
 }
--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom'
-import { ReactComponent as Logo } from '../../../../public/images/Logo.svg';
+import { ReactComponent as LogoSite } from '../../../images/Logo.svg';
+
+import styles from './Logo.module.scss'
 
 const Logo = () => {
     return (
-    
-        <Link to='/'>
-            <span>KidsLike</span>
-            <Logo />
-        </Link>
+    <>
+        
+        <div className={styles.logoWrapper}>
+            <Link to='/'>
+                <span className={styles.logo}>KidsLike</span>
+            </Link>
+            <LogoSite className={styles.logoSVG}/>
+        </div>
+           
+        
+        </>
             
+
     )
 }
 
