d5e8e803534c2de1520a828b1245af21526f16b8
Вікторія <viktoriaa@MacBook-Air-Viktoria.local>
2021-05-25 17:42:23 +0300

    created tasks list markup and styles

---
+++ src/client/Tasks/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import moment from 'moment'
+import { ReactComponent as Completed } from '../../../../images/completed.svg'
+import { ReactComponent as Incompleted } from '../../../../images/incompleted.svg'
+
+import styles from './TaskCard.module.scss'
+
+const TaskCard = ({ title, reward, imageUrl, date, isCompleted, toggleCompleted }) => {
+
+    const today = moment().format('YYYY-MM-DD');
+    const expiredDate = today > date;
+    const exactDate = today === date;
+
+    return (
+        <li className={styles.taskItem}>
+            <div>
+                <img src={imageUrl} alt={title} className={styles.taskImage} />
+            </div>
+            <div className={styles.taskDescription}>
+                <div>
+                    <h3 className={styles.taskTitle}>{title}</h3>
+                    <p className={styles.taskScore}>{reward} балла</p>
+                </div>
+                {exactDate && <input onChange={toggleCompleted}></input>}
+                <div>
+                    {expiredDate && isCompleted && <Completed />}
+                    {expiredDate && !isCompleted && <Incompleted />}
+                </div>
+            </div>
+        </li>
+    );
+}
+
+TaskCard.defaultProps = {
+    title: '',
+    reward: '',
+    imageUrl: '',
+    date: '',
+    isCompleted: false,
+    toggleCompleted: () => { }
+}
+
+TaskCard.propTypes = {
+    title: PropTypes.string,
+    reward: PropTypes.string,
+    imageUrl: PropTypes.string,
+    date: PropTypes.string,
+    isCompleted: PropTypes.bool,
+    toggleCompleted: PropTypes.func,
+}
+
+export default TaskCard;
