d6a86028a01824f033a46045c6e5f7215fcd04f5
Mariia Hunko <masha.gunko96@gmail.com>
2021-05-25 12:01:29 +0300

    Fix props transition in Modal

--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -3,14 +3,14 @@ import { createPortal } from 'react-dom';
 import { ReactComponent as CloseIcon } from './CloseIcon/close.svg';
 import styles from './Modal.module.scss';
 
-const Modal = ({ modalContent }) => {
+const Modal = ({ children }) => {
     const modalRoot = document.querySelector('#modal-root');
     return (
         createPortal(<div className={styles.backdrop}>
             <div className={styles.modalLayout}>
                 <div className={styles.modalContent}>
-                    <CloseIcon className={styles.closeBtnIcon}/>
-                    {modalContent}
+                    <CloseIcon className={styles.closeBtnIcon} />
+                    {children}
                 </div>
             </div>
         </div>, modalRoot)
