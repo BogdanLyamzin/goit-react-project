302012ebd4400c849f7776d647dfc9670361eccf
Mariia Hunko <masha.gunko96@gmail.com>
2021-05-25 16:58:04 +0300

    Added modal logic for open and close modal

--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -1,15 +1,44 @@
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import { ReactComponent as CloseIcon } from './CloseIcon/close.svg';
 import styles from './Modal.module.scss';
 
-const Modal = ({ children }) => {
+const Modal = ({ children, onClose }) => {
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onClose]);
+
+    const handleKeyDown = useCallback(
+        (e) => {
+            if (e.code === "Escape") {
+                onClose();
+            }
+        },
+        [onClose]
+    );
+
+    const handleClick = () => {
+        onClose();
+    };
+
+    const handleBackdropClick = (e) => {
+        console.log(e);
+        if (e.currentTarget === e.target) {
+            onClose();
+        }
+    };
+
+
     const modalRoot = document.querySelector('#modal-root');
     return (
-        createPortal(<div className={styles.backdrop}>
+        createPortal(<div className={styles.backdrop} onClick={handleBackdropClick}>
             <div className={styles.modalLayout}>
                 <div className={styles.modalContent}>
-                    <CloseIcon className={styles.closeBtnIcon} />
+                    <CloseIcon className={styles.closeBtnIcon} onClick={handleClick}/>
                     {children}
                 </div>
             </div>
