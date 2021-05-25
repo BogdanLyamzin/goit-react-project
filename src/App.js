9f8e014d7df05b67c6c44d58ad39f667fc24c6e8
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-05-25 15:36:29 +0300

    delete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import Button from './shared/components/Button'
-import CheckboxToggle from './shared/components/CheckboxToggle'
-import FormCheckbox from './shared/components/FormCheckbox'
-import FormInput from './shared/components/FormInput'
-import Logo from './shared/components/Logo'
 
 function App() {
   return ( <>
-      <Button children='click'/>
-      <CheckboxToggle/>
-      <FormCheckbox/>
-      <FormInput label='name'/>
-      <Logo/>
+     
       </>)
 }
 
