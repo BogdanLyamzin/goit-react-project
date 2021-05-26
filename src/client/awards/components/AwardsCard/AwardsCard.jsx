8c62a71e2a9291cdf8755d89d559e79cb24b7c49
Mariia Hunko <masha.gunko96@gmail.com>
2021-05-26 22:35:15 +0300

    Created new component Reward Modal and added styles to it

--- src/client/awards/components/AwardsCard/AwardsCard.jsx
+++ src/client/awards/components/AwardsCard/AwardsCard.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import {getAwards} from '../../../../redux/awards/awards-selectors';
 import AwardsTitle from '../AwardsTitle';
 import Button from '../../../../shared/components/Button';
 import CheckboxToggle from '../../../../shared/components/CheckboxToggle';
+import Modal from '../../../../shared/components/Modal';
+import AwardsModal from '../AwardsModal';
 import styles from './AwardsCard.module.scss';
 
 const AwardsCard = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const toggleModal = () => {
+        setOpenModal(!openModal);
+    };
+
     const awards = useSelector(state => getAwards(state), shallowEqual);
 
     const awardsList = awards.map(({ id, title, price, imageUrl }) =>
@@ -17,9 +24,9 @@ const AwardsCard = () => {
             <h3 className={styles.gift_name}>{title}</h3>
             <span className={styles.price}>{price} баллов</span>
             </div>
-            <div className={styles.container_checkboxtoggle}> <CheckboxToggle className={styles.checkbox_toggle}/></div> 
+            <div className={styles.container_checkboxtoggle}> <CheckboxToggle className={styles.checkbox_toggle}/></div>
             </div>
-            </li> )
+        </li> )
     return (
         <div className={styles.container}>
             <div className={styles.container_awards}>
@@ -28,15 +35,19 @@ const AwardsCard = () => {
                 {/* <div className={styles.rating}>
                     <p className={styles.text}>Заработано баллов за эту неделю: <span className={styles.number_bold}>8</span></p>
                     <p className={styles.text}>Запланированно баллов на эту неделю: <span className={styles.number_bold}>16</span></p>
-                    <span className={styles.number_rate}>8 <span className={styles.number}>/ 16</span></span> 
+                    <span className={styles.number_rate}>8 <span className={styles.number}>/ 16</span></span>
                     <progress className={styles.rate_line} value='0' max='100'></progress>
                 </div> */}
             </div>
             <ul className={styles.list}>
                  {awardsList}
             </ul>
-            </div> 
-         <Button className={styles.button_confirm}> Подтвердить </Button>   
+        </div>
+        {openModal && (
+            <Modal onClose={toggleModal}>
+                <AwardsModal />
+            </Modal>)}
+        <Button type="submit" className={styles.button_confirm} onClick={toggleModal}> Подтвердить </Button>
         </div>
     )
 };
